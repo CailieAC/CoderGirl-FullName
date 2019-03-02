32ad929bdbd1242edbefc7a0d6f2a71ba6961002
Carlile Upfront <carlileupfront@users.noreply.github.com>
2019-03-02 14:58:24 -0600

    commit1

--- FullName/Program.cs
+++ FullName/Program.cs
@@ -8,16 +8,23 @@ namespace FullName
         {
             // TODO: Ask the user for their first name and then their last name.
             Console.Write("Enter your first name: ");
-            string firstName
+            string firstName = Console.ReadLine();
             Console.Write("Enter your last name: ");
+            string lastName = Console.ReadLine();
 
             // TODO: Call a method called FullName that returns their full name.
+            string fullName = FullName(firstName, lastName);
 
             // TODO: Output their full name.
-
+            Console.WriteLine(fullName);
             Console.ReadLine();
         }
 
         // TODO: Create a method call FullName that takes the firstName and lastName as parameters.
+        public static string FullName(string firstName, string LastName)
+        {
+            string fullName = firstName + " " + LastName;
+            return fullName;
+        }
     }
 }
